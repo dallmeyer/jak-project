4d243fa6fe2c44ceea0b77f8e1e3b5be9552f101
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2022-07-19 00:55:41 -0700

    kinda broken

--- goal_src/jak1/dgos/game.gd
+++ goal_src/jak1/dgos/game.gd
@@ -264,6 +264,7 @@
   ("water.o" "water")
   ("collectables-part.o" "collectables-part")
   ("collectables.o" "collectables")
+  ("checkpoint-randomizer.o" "checkpoint-randomizer")
   ("task-control.o" "task-control")
   ("process-taskable.o" "process-taskable")
   ("pov-camera.o" "pov-camera")
